c6fe28719be7ef2c9ad78f96974ea19a449104ec
Cong Zhang <dancerphil1994@gmail.com>
2019-05-23 13:55:19 +0800

    Update react version

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 const factory = (targetComponent, targetName) => {
-  return class extends Component {
-    render() {
-      const {children} = this.props;
-      return (
-        <Fragment>
-          <div>{`children.type === ${targetName}: ${children.type === targetComponent}`}</div>
-          <div>{`children.type.name === '${targetName}': ${children.type.name === targetName}`}</div>
-          <div>{`children.type.displayName === '${targetName}': ${children.type.displayName === targetName}`}</div>
-        </Fragment>
-      );
-    }
-  }
+  const Display = ({children}) => (
+    <Fragment>
+      <div>{`children.type === ${targetName}: ${children.type === targetComponent}`}</div>
+      <div>{`children.type.name === '${targetName}': ${children.type.name === targetName}`}</div>
+      <div>{`children.type.displayName === '${targetName}': ${children.type.displayName === targetName}`}</div>
+    </Fragment>
+  );
+  return Display;
 }
 
 class ClassComponent extends Component {}
@@ -26,25 +22,21 @@ FunctionalComponent.displayName = 'FunctionalComponent';
 const IsMyChildClassComponent = factory(ClassComponent, 'ClassComponent')
 const IsMyChildFunctionalComponent = factory(FunctionalComponent, 'FunctionalComponent')
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <div>ClassComponent</div>
-          <IsMyChildClassComponent>
-            <ClassComponent />
-          </IsMyChildClassComponent>
-          <div>------</div>
-          <div>FunctionalComponent</div>
-          <IsMyChildFunctionalComponent>
-            <FunctionalComponent />
-          </IsMyChildFunctionalComponent>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <div>ClassComponent</div>
+      <IsMyChildClassComponent>
+        <ClassComponent />
+      </IsMyChildClassComponent>
+      <div>------</div>
+      <div>FunctionalComponent</div>
+      <IsMyChildFunctionalComponent>
+        <FunctionalComponent />
+      </IsMyChildFunctionalComponent>
+    </header>
+  </div>
+);
 
 export default App;
